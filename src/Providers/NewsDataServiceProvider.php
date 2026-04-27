9745a156b47171b009f316962c156baf9a506cfa
mikeyperes <mikeyperes@users.noreply.github.com>
2026-04-27 03:29:37 +0000

    feat(newsdata): register package metadata

--- src/Providers/NewsDataServiceProvider.php
+++ src/Providers/NewsDataServiceProvider.php
@@ -41,6 +41,13 @@ class NewsDataServiceProvider extends ServiceProvider
             // HWS-SIDEBAR-MENU-3L-END
 
             $registry->registerSidebarLink('newsdata.index', 'NewsData', 'M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z', 'Sandbox', 'newsdata', 82);
+            $registry->registerPackage('newsdata', 'hexawebsystems/laravel-hexa-package-newsdata', [
+                'title' => 'NewsData',
+                'description' => 'NewsData source configuration for discovery and article sourcing.',
+                'icon' => 'M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z',
+                'color' => 'sky',
+                'settingsRoute' => 'settings.newsdata',
+            ]);
         }
 
         // Settings card on /settings page
